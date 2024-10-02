ab716be6006bd4197b5ae506ee85d95cc867fd26
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-02 20:14:19 +0800

    Implement repeatString function
    - Updated `repeatString` function that repeats a
    given string based on the provided number.
    - Returns "ERROR" if the number is less than 0.
    - Handles edge case where num is 0 by returning an empty string.
    - Uses a while loop to concatenate the string until the desired number
    is reached.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else if (num === 0) {
+        return "";
+    } else {
+        let newstring = string;
+        while (num > 1) {
+            newstring += string;
+            num--;
+        }
+    return newstring;
+    }
 };
 
 // Do not edit below this line
