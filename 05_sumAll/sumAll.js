e3bb29539fcd887c8bf5e614107118827e10f9a8
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-03 21:23:52 +0800

    Implement sumAll function
    - Added code to `sumAll` function to calculate the sum of all numbers
    between two given integers, inclusive.
    - Handles cases where the start value is greater than the end value.
    - Returns "ERROR" for invalid inputs, including negative numbers,
    non-integer values, and non-numeric types.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let finalSum = 0;
+    switch (true) {
+        case (start < 0 
+            || end < 0 
+            || start != Math.floor(start)
+            || end != Math.floor(end)
+            || typeof start != "number"
+            || typeof end != "number"):
+            return "ERROR";
+            break;
+        case (start > end):
+            for (i = start; i >= end; i--) {
+                finalSum += i;
+            }
+            return finalSum;
+            break;
+        case (start < end):
+            for (i = start; i <= end; i++) {
+                finalSum += i;
+            }
+            return finalSum;
+            break;
+    }
 };
 
 // Do not edit below this line
