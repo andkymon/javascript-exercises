5127333e0e88e613cb6e4edc2941f228b71e2d54
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-03 22:04:06 +0800

    implement temperature conversion functions
    - Added code to `convertToCelsius` function to convert Fahrenheit to
    Celsius, rounding to 1 decimal place unless the result is an integer.
    - Added code to `convertToFahrenheit` function to convert Celsius to
    Fahrenheit, also rounding to 1 decimal place unless the result is an
    integer.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) * 5 / 9;
+  if (Number.isInteger(c) === true) {
+    return Number(c.toFixed(0));
+  } else {
+    return Number(c.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c * (9/5) + 32;
+  if (Number.isInteger(f) === true) {
+    return Number(f.toFixed(0));
+  } else {
+    return Number(f.toFixed(1));
+  }
 };
 
 // Do not edit below this line
