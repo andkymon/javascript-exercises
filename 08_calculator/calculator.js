8f0490a6d334dd1e3365b945ff1ca601907bdffa
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-13 11:06:53 +0800

    Finish javascript exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, num) => {
+    return product * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const array = [];
+  for (i = num; i > 0; i--) {
+    array.push(i);
+  }
+	return array.reduce((product, num) => {
+    return product * num;
+  }, 1);
 };
 
 // Do not edit below this line
