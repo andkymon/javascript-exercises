f5ca454b5123801216ffacf1ff11008fd9188a20
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-13 11:46:18 +0800

    Finish javascript exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const array = str
+    .toLowerCase()
+    .split("")
+    .filter((item) => item.match(/^[a-zA-Z0-9]+$/));
 
+    return array.join("") === array.reverse().join("");
 };
 
 // Do not edit below this line
