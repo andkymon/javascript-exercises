8bd248e13259d89f518ee26a0e12726516602a0c
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-13 13:36:46 +0800

    Finish javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    const array = [0, 1];
+    while (array.length <= num) {
+        let newnum = array[array.length - 1] + array[array.length - 2];
+        array.push(newnum);
+    }
+    return array[num];
 };
 
 // Do not edit below this line
